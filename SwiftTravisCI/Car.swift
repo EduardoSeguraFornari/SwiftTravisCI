cbca8c1f4bdf10b7c33c0f42364c571c65b392ad
Eduardo Segura Fornari <eduardo.fornari@icloud.com>
2018-11-16 14:02:08 -0200

    Adding CarTests

---
+++ SwiftTravisCI/Car.swift
@@ -0,0 +1,9 @@
+//
+//  Car.swift
+//  SwiftTravisCI
+//
+//  Created by Eduardo Fornari on 16/11/18.
+//  Copyright © 2018 Some Company. All rights reserved.
+//
+
+import Foundation
---
+++ SwiftTravisCITests/CarTests.swift
@@ -0,0 +1,33 @@
+//
+//  CarTests.swift
+//  SwiftTravisCITests
+//
+//  Created by Eduardo Fornari on 16/11/18.
+//  Copyright © 2018 Some Company. All rights reserved.
+//
+
+import XCTest
+
+class CarTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
